2d3d23571bea2ebdb3deee1ee1f3053d1df188f4
TanWentao <twt01@qq.com>
2024-04-11 19:14:35 +0800

    [bug fix] Add UserController to route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IdeaController;
@@ -9,6 +10,7 @@ Route::get('/', function () {
 });
 
 Route::resource('idea', IdeaController::class)->middleware('auth');
+Route::resource('user', UserController::class)->middleware('auth');
 
 Route::get('/searchByDestination', 'IdeaController@searchByDestination');
 Route::get('/searchByTag', 'IdeaController@searchByTag');
