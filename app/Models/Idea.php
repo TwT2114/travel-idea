0e6d591862b4c36009b476370272ded093e1f265
TanWentao <twt01@qq.com>
2024-04-14 11:58:41 +0800

    Add soft deletes and edit/delete idea functionality

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Comment;
+use App\Http\Response;
+use Request;
 
 class Idea extends Model
 {
     use HasFactory;
+
+    use SoftDeletes;
 
     public function comments()
     {
