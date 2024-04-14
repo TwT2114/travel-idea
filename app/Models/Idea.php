9d47bfd6dfd2d9f5661b91621af34bb963a4bee6
TanWentao <twt01@qq.com>
2024-04-14 12:06:06 +0800

    feat: Remove unused imports from Idea model

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Models\Comment;
-use App\Http\Response;
-use Request;
+
 
 class Idea extends Model
 {
