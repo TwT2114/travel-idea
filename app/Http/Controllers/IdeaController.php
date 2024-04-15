6419107f46eba60aa8cae808d486b024826dde3f
TwT2114 <2564588957@qq.com>
2024-04-15 22:57:06 +0800

    feat: Moved WEATHER_API_KEY to separate config entry and retrieved it using config

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -237,7 +237,7 @@ class IdeaController extends Controller
     }
     public function getCityWeather($destination)
     {
-        $apiKey = env('WEATHER_API_KEY');
+        $apiKey = config('api.weather');
         $getGeoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" . $destination . "&limit=1&appid=" . $apiKey;
         $geoData = Http::get($getGeoUrl)->throw()->json();
         $lon = $geoData[0]['lon'];
--- config/api.php
+++ config/api.php
@@ -3,6 +3,8 @@
 return [
 
     'google_map' => env('GOOGLE_MAP_API'),
+    'weather' => env('WEATHER_API_KEY'),
+
 
 ];
 
