01a306526b66282170b9afed86f1de26fea46c46
yxq <qxt19960122@163.com>
2024-04-16 16:46:55 +0800

    Implemented weather feature with error handling for ideas controller.

---
+++ public/js/buttonShow.js
@@ -0,0 +1,8 @@
+function toggleIdeas() {
+    var ideaSection = document.getElementById('ideaSection');
+    if (ideaSection.style.display === "none") {
+        ideaSection.style.display = "block";
+    } else {
+        ideaSection.style.display = "none";
+    }
+}
