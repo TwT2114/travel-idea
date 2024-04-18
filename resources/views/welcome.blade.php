85a0539b934ffbb08488d63f2f5a2c7caa6615e7
TwT2114 <2564588957@qq.com>
2024-04-18 10:01:03 +0800

    feat: update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,13 @@
 @section('content')
     <article>
         <div><h1>Welcome to Travel Idea, {{ \Illuminate\Support\Facades\Auth::user()->name }}</h1></div>
+        <div>
+            <p>
+                You can post your travel ideas,
+                chat with friends under the idea,
+                and create travel plans with ideas.
+            </p>
+        </div>
     </article>
 @endsection
 
