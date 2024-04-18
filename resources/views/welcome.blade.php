bcc789996fe553434bfdfbd36317941924bb78b6
TwT2114 <2564588957@qq.com>
2024-04-18 11:32:30 +0800

    feat(views): Update title and add login/register links to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,41 @@
 @endsection
 
 @section('content')
-    <article>
-        <div><h1>Welcome to Travel Idea, {{ \Illuminate\Support\Facades\Auth::user()->name }}</h1></div>
+    @guest()
         <div>
-            <p>
-                You can post your travel ideas,
-                chat with friends under the idea,
-                and create travel plans with ideas.
-            </p>
+            <article>
+                <h1>Welcome to {{ config('app.name', 'Travel Idea') }}, please
+
+                        @if (Route::has('login'))
+                            <p class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </p>
+                        @endif
+                        /
+                        @if (Route::has('register'))
+                            <p class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </p>
+                        @endif
+                </h1>
+                <p>
+                    You can post your travel ideas,
+                    chat with friends under the idea,
+                    and create travel plans with ideas.
+                </p>
+            </article>
         </div>
-    </article>
+    @else
+        <article>
+            <div><h1>Welcome to Travel Idea, {{ \Illuminate\Support\Facades\Auth::user()->name }}</h1></div>
+            <div>
+                <p>
+                    You can post your travel ideas,
+                    chat with friends under the idea,
+                    and create travel plans with ideas.
+                </p>
+            </div>
+        </article>
+    @endguest
 @endsection
 
